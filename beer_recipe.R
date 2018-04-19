0b183822bcc7102b7dcdb3c438da67189fbcb9a2
Oguzhan Yediel <oguzhan.yediel@gmail.com>
2018-04-20 01:01:15 +0300

    Definitive Commit

---
+++ beer_recipe.R
@@ -0,0 +1,185 @@
+used_packages <- c("Amelia", "dplyr", "ggplot2", "futile.logger", "readr", 
+                   "reshape", "reshape2")
+lapply(used_packages, require, character.only = TRUE)
+
+recipeData <- read_csv(
+  "~/recipeData.csv", 
+  na = "N/A")
+
+# row count and length of data
+nrow(recipeData)
+length(recipeData)
+
+# dimension of data (row count + length)
+dim(recipeData)
+
+# general info about data
+summary(recipeData)
+
+# the first six and last six row of data
+head(recipeData) 
+tail(recipeData)
+
+# columns names
+colnames(recipeData)
+
+# missing data visualization
+missmap(head(recipeData, 500))
+missmap(recipeData)
+flog.info("It takes the time to appear the plot/graphic/image, 
+          please wait a little!")
+
+# Priming Method & Priming Amount
+null_priming <- table(is.na(recipeData$PrimingMethod))[2][["TRUE"]]
+sprintf("Priming Method is null on %s rows out of %s, so %s percent of the time",
+        null_priming, length(recipeData$PrimingMethod),
+        round((null_priming/length(recipeData$PrimingMethod))*100, 2))
+
+style_cnt <- recipeData[,c("Style", "PrimingMethod")]
+style_cnt$NullPriming <- is.na(recipeData$PrimingMethod)
+style_cnt$Count <- as.numeric(1)
+
+style_cnt_grp <- 
+  style_cnt %>% 
+  group_by(Style) %>% 
+  summarise(Count=sum(Count), NullPriming = sum(NullPriming))
+
+style_cnt_grp <- style_cnt_grp[order(-style_cnt_grp$NullPriming),]
+
+if(FALSE){
+  height <- as.matrix(style_cnt_grp[1:20,][,c("Count")])
+  barplot(height = height,
+          beside = TRUE,
+          # legend.text = c(style_cnt_grp[1:20,][,c("Style")]),
+          legend.text = TRUE,
+          main = "Missing Values in PrimingMethod column, per style",
+          xlab = "Count of Beer Recipes",
+          horiz = TRUE)
+}
+
+
+# Please, look at the graphics in bigger screen :)
+ggplot(style_cnt_grp[1:20,]) +
+  geom_bar(mapping = aes(x=Style, y=Count, fill=NullPriming),
+           stat = "identity", 
+           position = position_stack(reverse = TRUE)) +
+  geom_text(aes(x=Style, y=Count, label = paste("NullP:",NullPriming, sep=" ")), 
+            size = 3, position = position_stack(vjust = 0.5)) +
+  coord_flip() + 
+  theme_bw() +
+  labs(title = "Missing Values in PrimingMethod column, per style", 
+       x = "Style", 
+       y = "Count of Beer Recipes", 
+       fill = "Priming Method is null")
+# Part of geom_text is optional. 
+# If you do not want to see, you can remove from the ggplot code.
+
+ggplot(style_cnt_grp[1:20,], aes(x=Style, y=Count, fill=NullPriming)) +
+  geom_bar(stat = "identity", position = position_stack(reverse = TRUE)) +
+  facet_wrap(~ NullPriming) +
+  coord_flip() + 
+  theme_bw() +
+  labs(title = "Missing Values in PrimingMethod column, per style", 
+       x = "Style", y = "Count of Beer Recipes", fill = "Priming Method is null")
+
+# Categorical Features
+sapply(colnames(recipeData), function(x) class(recipeData[[x]]))
+
+recipeData[, sapply(recipeData, class) == 'character']
+recipeData[sapply(recipeData, function(recipeData) !is.numeric(recipeData))]
+# Two sentences have same result
+
+number_of_chr <- table(sapply(colnames(recipeData), 
+                              function(x) class(recipeData[[x]])))
+flog.info("Recipe Data has %s chr/factor attr", number_of_chr[["character"]])
+
+# That's the seven features:
+chrs <- sapply(recipeData, is.character)
+chrCols <- names(recipeData[, chrs])
+chrCols
+
+unique(recipeData$PrimingAmount)
+unique(recipeData$SugarScale)
+
+ggplot(recipeData, aes(SugarScale)) +
+  scale_x_discrete(limits=c("Specific Gravity", "Plato")) +
+  geom_bar(aes(fill = SugarScale)) +
+  theme(legend.position = "none") +
+  labs(title = "Frequency table of possible values in SugarScale")
+
+flog.info("SugarScale has %s null values", sum(is.na(recipeData$SugarScale)))
+
+ggplot(recipeData, aes(BrewMethod)) +
+  geom_bar(aes(fill = BrewMethod)) +
+  theme(legend.position = "none") +
+  labs(title = "Frequency table of possible values in BrewMethod")
+
+flog.info("BrewMethod has %s null values", sum(is.na(recipeData$BrewMethod)))
+flog.info("PrimingMethod has %s unique values", 
+          length(unique(recipeData$PrimingMethod)))
+print((unique(recipeData$PrimingMethod))[1:20])
+
+recipeData[sapply(recipeData, function(recipeData) !is.character(recipeData))]
+num <- sapply(recipeData, is.numeric)
+numCols <- names(recipeData[, num])
+numCols
+
+get_sg_from_plato <- function(plato) {
+  sg <- 1 + (plato / (258.6 - ( (plato / 258.2) * 227.1) ) )
+  return(sg)
+}
+
+recipeData$OG_sg <- NA
+for(i in 1:length(recipeData$SugarScale)) {
+  recipeData$OG_sg[i] <- ifelse(recipeData$SugarScale[i] == 'Plato',
+                                get_sg_from_plato(recipeData$OG[i]),
+                                recipeData$OG[i]) 
+}
+
+recipeData$FG_sg <- NA
+for(i in 1:length(recipeData$SugarScale)) {
+  recipeData$FG_sg[i] <- ifelse(recipeData$SugarScale[i] == 'Plato',
+                                get_sg_from_plato(recipeData$FG[i]),
+                                recipeData$FG[i])
+}
+
+recipeData$BoilGravity_sg <- NA
+for(i in 1:nrow(recipeData)){
+  recipeData$BoilGravity_sg[i] <- ifelse(recipeData$SugarScale[i] == 'Plato',
+                                         get_sg_from_plato(recipeData$BoilGravity[i]),
+                                         recipeData$BoilGravity[i])
+}
+
+num_feats_list = recipeData[,c('Size(L)', 'OG_sg', 'FG_sg', 'ABV', 'IBU',
+                               'Color', 'BoilSize', 'BoilTime', 'BoilGravity_sg',
+                               'Efficiency', 'MashThickness', 'PitchRate', 
+                               'PrimaryTemp')]
+summary(num_feats_list)
+quantile(num_feats_list$BoilSize, c(0.25, 0.5, 0.75), type = 8)
+quantile(num_feats_list$BoilTime, c(0.25, 0.5, 0.75), type = 1)
+# For detailed info: 
+# https://tolstoy.newcastle.edu.au/R/e17/help/att-1067/Quartiles_in_R.pdf
+
+# should be defined a function that will categorize the features automatically
+vlow_scale_feats = recipeData[,c('OG_sg', 'FG_sg', 'BoilGravity_sg', 'PitchRate')]
+low_scale_feats = recipeData[,c('ABV', 'MashThickness')]
+mid_scale_feats = recipeData[,c('Color', 'BoilTime', 'Efficiency', 'PrimaryTemp')]
+high_scale_feats = recipeData[,c('IBU', 'Size(L)',  'BoilSize')]
+
+boxplot(vlow_scale_feats, value~variable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
